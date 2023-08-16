96937ec297cd7186d8c82b3037ec4cd2639181ca
Eric Charbonnier <eric2503@hotmail.com>
2023-08-16 18:35:55 -0400

    ajustement pour composants Expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "../NewExpense/ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -39,77 +40,11 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses}/>
-
-        {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
-        {filteredExpenses.length > 0 &&
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              amount={expense.amount}
-              date={expense.date}
-              title={expense.title}
-            />
-          ))} */}
-
-        {/* {filteredExpenses.length === 0 ? (
-          <p>No expenses found.</p>
-        ) : (
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              amount={expense.amount}
-              date={expense.date}
-              title={expense.title}
-            />
-          ))
-        )} */}
       </Card>
     </div>
   );
 };
 
 export default Expenses;
-
-// function Expenses(props) {
-//   console.log(props.items); // Affiche le tableau des dépenses
-//   return (
-//     <div className="expenses">
-//       {props.items.map((expense) => (
-//         <ExpenseItem
-//           key={expense.id}
-//           amount={expense.amount}
-//           date={expense.date}
-//           title={expense.title}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
-
-// function Expenses(props) {
-//   return (
-//     <div className="expenses">
-//       <ExpenseItem
-//         title={props.items[0].title}
-//         amount={props.items[0].amount}
-//         date={props.items[0].date}
-//       />
-//       <ExpenseItem
-//         title={props.items[1].title}
-//         amount={props.items[1].amount}
-//         date={props.items[1].date}
-//       />
-//       <ExpenseItem
-//         title={props.items[2].title}
-//         amount={props.items[2].amount}
-//         date={props.items[2].date}
-//       />
-//       <ExpenseItem
-//         title={props.items[3].title}
-//         amount={props.items[3].amount}
-//         date={props.items[3].date}
-//       />
-//     </div>
-//   );
-// }
